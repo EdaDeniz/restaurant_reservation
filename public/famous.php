574301cd881d3a827c4dd81504869a9a39236f0e
Eda Deniz Çelik <42175967+EdaDeniz@users.noreply.github.com>
2020-06-16 23:34:33 +0300

    Add files via upload

---
+++ public/famous.php
@@ -0,0 +1,87 @@
+<?php $uesrType = 2?>
+<html>
+
+<head>
+    <title>Çelikler Restaurant</title>
+
+    <!-- CSS FILES -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">    
+    <link href="css/fam.css" rel="stylesheet">    
+    
+
+
+    <!-- TAB ICON -->
+    <link href="images/logo-icon.png" rel="icon">
+
+    <!-- JavaScript and jQuery files -->
+    <script src="js/jquery-1.12.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/home.js"></script>
+    <script src="js/index.js"></script>
+    <script src="js/fam.js"></script>
+
+
+</head>
+
+
+<body id="index-page-body" style="background-color: #232934">
+    <!-- Overflow black background -->
+    <div class="background"></div>
+
+    <!-- Home section -->
+    <div class="home-section">
+        <!-- Nav bar starts -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="images/logo.png" class="logo" id="index-page" /></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php" style="border: solid"> Back Home 👉 </a></li>
+                       
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+
+    <br><br>
+    <br><br>
+    <br><br>
+    <!-- IMAGES start -->
+    <div>
+        
+
+            <div class="card-holder">
+    <div class="card" id="c"><img src="https://www.washingtonpost.com/resizer/ntC2kBCaKsNol_rGUHGM20joW-I=/1484x0/arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/MGOI3YCJ3Y4RDE5LAU66U4Q3PE.jpg"><img></div>
+    <div class="card"><img src="https://identity-mag.com/wp-content/uploads/2019/08/mac.jpg" alt=""></div>
+    <div class="card"><img src="https://i.redd.it/wmlw5dw3tq151.jpg" alt="Çok sevgili Ender hoca"></div>
+    <div class="card"><img src="https://media-cdn.t24.com.tr/media/stories/2019/01/raw_karatay-12-yil-amerikada-yasadim-hic-hamburger-yemedim_628800418.jpg" alt=""></div>
+    <div class="card"><img src="https://i.pinimg.com/originals/78/6d/98/786d9870800665cd2d0348c2549c7d7d.jpg" alt=""></div>
+    <div class="card"><img src="https://hips.hearstapps.com/del.h-cdn.co/assets/16/05/1454616726-giphy-1.gif" alt=""></div>
+    <div class="card"><img src="https://cdn.i-scmp.com/sites/default/files/styles/1200x800/public/d8/images/methode/2019/04/01/0cf192da-4edd-11e9-8617-6babbcfb60eb_image_hires_144915.jpg?itok=uiHKztfJ&v=1554101363" alt=""></div>
+    <div class="card"><img src="https://66.media.tumblr.com/b5e4220e134a5f3817f05b01d9aa62d0/tumblr_mywrrbfIFh1so9fmco1_500.gif" alt=""></div>
+    <div class="card"><img src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr01/2013/5/10/19/anigif_enhanced-buzz-28135-1368228252-21.gif?downsize=600:*&output-format=auto&output-quality=auto" alt=""></div>
+    <div class="card"><img src="https://i.imgur.com/pLg49Qp.jpg" alt=""></div>
+    <div class="card"><img src="https://pbs.twimg.com/media/DCCpwngXkAAx99j.jpg" alt=""></div>
+  </div>
+
+    </div>
+        <!-- IMAGES end -->
+
+</body>
+ 
+</html>
